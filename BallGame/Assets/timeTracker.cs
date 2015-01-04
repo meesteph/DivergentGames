5e1ac089581c2867957f5b1278fbb5f7794125aa
Stephen Mee <meesteph@msu.edu>
2015-01-04 16:07:23 -0500

    Created timeTracker script

---
+++ BallGame/Assets/timeTracker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class timeTracker : MonoBehaviour {
+
+	public static float timer;
+	
+	// Update is called once per frame
+	void Update () {
+		Text gameTime = GetComponent<Text> ();
+		timer = Time.fixedTime;
+		gameTime.text = "Timer: " + timer.ToString();
+	}
+}
