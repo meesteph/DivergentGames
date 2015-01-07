bdcfef310fc551ad00f46b57c78a2e138582e631
broadway <wittekindmark@gmail.com>
2015-01-07 02:56:44 -0500

    Added logo/icon
    "BallGame" text can and will be changed when we name the game.

--- BallGame/Assets/Scripts/playerDie.cs
+++ BallGame/Assets/Scripts/playerDie.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class playerDie : MonoBehaviour {
 
-	private int lives;
+    public static bool gamePaused = false;
+    private int lives;
     public GameObject player;
 
-	void OnTriggerEnter (Collider playerSphereCollider){
+    void OnTriggerEnter (Collider playerSphereCollider){
 
         Destroy(playerSphereCollider);
         
-		if(WorldScript.playerLives > 0) {
+        if(WorldScript.playerLives > 0) {
 
             // Decrement lives
             WorldScript.playerLives -= 1;
@@ -23,5 +24,21 @@ public class playerDie : MonoBehaviour {
         } else {
             // TODO Create 'new game' menu (also shows latest score and top scores)
         }
-	}
+
+        Destroy(playerSphereCollider);
+        PauseGame();
+        // Upload time to database
+        timeTracker.timer = 0.0f;
+    }
+    
+    public void PauseGame()
+    {
+        // Pause game
+        Time.timeScale = 0;
+        gamePaused = true;
+        
+        // Instantiate menu scene
+        Application.LoadLevel ("SettingsScene");
+
+    }
 }
